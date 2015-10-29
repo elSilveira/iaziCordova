eb90af9b6193b72b875b1e29c3a1481e2b9c17b4
elSilveira <duzitz@gmail.com>
2015-10-29 14:22:21 -0200

    Commit add class assist

---
+++ iaziCordova/www/scripts/Assistent.js
@@ -0,0 +1,54 @@
+﻿function retornaCor(posicao) {
+    while (posicao > 4) {
+        posicao = posicao - 5;
+    }
+    switch (posicao) {
+        case (0):
+            return 'ff726e';
+        case (1):
+            return 'dd5758';
+        case (2):
+            return 'e8494e';
+        case (3):
+            return 'd4393d';
+        case (4):
+            return 'bc2c35';
+    }
+}
+
+function retornaCorOver(posicao) {
+    while (posicao > 4) {
+        posicao = posicao - 5;
+    }
+    switch (posicao) {
+        case (0):
+            return 'ff728e';
+        case (1):
+            return 'dd5778';
+        case (2):
+            return 'e8496e';
+        case (3):
+            return 'd4395d';
+        case (4):
+            return 'bc2c55';
+    }
+}
+//Controla as mudanças de pagina e cliques
+function nextPage(page, data) {
+    switch (page) {
+        case (0):
+            $(".ui-content").empty();
+            $(".ui-content").append("<ul id='listCategorias' style='width:100%; height:100%; list-style: none; margin: 0; padding: 0;'></ul>");
+            getServicos();
+            break;
+        case (1):
+            $("#main").empty();
+            break;
+        case (0):
+            $("#main").empty();
+            break;
+        default:
+            break;
+    }
+}
+
